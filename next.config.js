29afc1fa576fb6b007bb7e802029d0eb1e62fd2a
Rio Anggara <rioanggaraclub@gmail.com>
2024-10-27 10:45:03 +0700

    Add error handling and loggigggng

--- next.config.js
+++ next.config.js
@@ -1,23 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
-  // Add this to see more detailed error messages during build
-  webpack: (config, { dev, isServer }) => {
-    if (dev && isServer) {
-      config.infrastructureLogging = {
-        level: 'verbose',
-      }
-    }
-    return config
-  },
-}
+   /** @type {import('next').NextConfig} */
+   const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: '**',
+        },
+      ],
+    },
+  }
 
-module.exports = nextConfig
+  module.exports = nextConfig
